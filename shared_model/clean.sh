a1c23c9a399b2bfbe578ad207e5252c2d72224e7
Victor Drobny <vdrobny@yandex.ru>
2018-03-12 11:28:33 +0300

    refactor
    Signed-off-by: Victor Drobny <vdrobny@yandex.ru>

--- shared_model/clean.sh
+++ shared_model/clean.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+rm ../schema/*.{cc,h}
 rm -rf external
 rm -rf build
--- shared_model/cmake/dependencies.cmake
+++ shared_model/cmake/dependencies.cmake
@@ -6,12 +6,6 @@ set_directory_properties(PROPERTIES
     EP_PREFIX ${EP_PREFIX}
     )
 
-if(CMAKE_GENERATOR MATCHES Xcode)
-  set(XCODE_EXT "/Debug")
-else()
-  set(XCODE_EXT "")
-endif()
-
 # Project dependencies.
 find_package(Threads REQUIRED)
 
@@ -29,20 +23,11 @@ find_package(optional)
 ##########################
 #         boost          #
 ##########################
-find_package(Boost 1.65.0 REQUIRED
-    COMPONENTS
-    )
+find_package(Boost 1.65.0 REQUIRED)
 add_library(boost INTERFACE IMPORTED)
 set_target_properties(boost PROPERTIES
     INTERFACE_INCLUDE_DIRECTORIES ${Boost_INCLUDE_DIRS}
-#    INTERFACE_LINK_LIBRARIES "${Boost_LIBRARIES}"
     )
-
-if(ENABLE_LIBS_PACKAGING)
-  foreach (library ${Boost_LIBRARIES})
-    add_install_step_for_lib(${library})
-  endforeach(library)
-endif()
 
 ###################################
 #          ed25519/sha3           #
